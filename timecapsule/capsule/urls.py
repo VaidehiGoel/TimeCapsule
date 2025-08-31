cc1034e9dd3e572c365432f6756538c8b39a57ec
Your Name <youremail@example.com>
2025-08-31 18:06:05 +0530

    add login and registration features

--- timecapsule/capsule/urls.py
+++ timecapsule/capsule/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
+from django.contrib import admin
 from . import views
 
 
     
 urlpatterns = [
+    path("admin/", admin.site.urls),
     path('', views.home, name='home'),
     path('future/', views.future, name='future'),
     path('opened/', views.opened, name='opened'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 
 ]
